e45408e9a9c3f859174a7b5c431b514c019848f1
James Cohen <james.cohen@gmail.com>
2018-07-10 00:30:16 +0100

    ugly but working

--- src/runner.php
+++ src/runner.php
@@ -6,7 +6,12 @@ $configFile = __DIR__ ."/../config/config.json";
 
 $runner = new Runner($configFile);
 $runner->prepare();
-$runner->sendMetrics();
+while(true) {
+    $runner->sendMetrics();
+    sleep(60);
+}
+$runner->end();
+
 
 
 class Runner {
@@ -21,14 +26,70 @@ class Runner {
     public function prepare()
     {
         // make sure we have the basic config available
+        $this->_log("Reading Config");
         if (! $this->_config->read()) {
+            $this->_log("No Config Exists");
             $this->_getConfigValuesFromUser();
-            $this->_config->write();
+        }
+
+        if (empty($this->_config->hiveSessionId)) {
+            $this->_refreshHiveSession();
         }
     }
 
     public function sendMetrics()
     {
+        $hiveFactory = new HiveApiFactory();
+        $hive = $hiveFactory->getHiveApi($this->_config->hiveSessionId);
+
+        $this->_log("Fetching Hive data");
+        $response = $hive->get("nodes");
+        if (401 == $response->info->http_code) {
+            $this->_log("Initial Hive data fetch failed");
+            $this->_refreshHiveSession();
+            $this->_log("Fetching Hive data again");
+            $response = $hive->get("nodes");
+        }
+
+        if (200 != $response->info->http_code) {
+            $this->_log("Failed to fetch Hive node data");
+            $this->_log(print_r($response->response,true));
+        } else {
+            $a = json_decode($response->response);
+
+            $metrics = array();
+            foreach ($a->nodes as $node) {
+
+                if ($node->nodeType == "http://alertme.com/schema/json/node.class.light.json#") {
+                    $lightStatus = $node->attributes->state->reportedValue;
+                    $lightName = strtolower($node->name);
+
+                    if ("ON" == $lightStatus) {
+                        $brightness = $node->attributes->brightness->reportedValue;
+                    } else {
+                        $brightness = 0;
+                    }
+                    $metrics["hive.light.$lightName.brightness"] = $brightness; 
+                }
+
+                if (isset($node->attributes->stateHotWaterRelay->reportedValue)) {
+                    $metrics['hive.hotWaterRelay'] = $node->attributes->stateHotWaterRelay->reportedValue == "ON" ? 1 : 0;
+                }
+                if (isset($node->attributes->stateHeatingRelay->reportedValue)) {
+                    $metrics['hive.heatingRelay'] = $node->attributes->stateHeatingRelay->reportedValue == "ON" ? 1 : 0;
+                }
+            }
+
+            $dataDog = new DatadogHelper($this->_config->datadogApiKey);
+            $this->_log("Sending data for ". count($metrics) ." metrics to Datadog");
+            $dataDog->sendMetrics($metrics);
+        }
+    }
+
+    public function end()
+    {
+        $this->_config->write();
+        $this->_log("Exiting...");
     }
 
     private function _getConfigValuesFromUser()
@@ -41,6 +102,7 @@ class Runner {
 
     private function _refreshHiveSession()
     {
+        $this->_log("Fetching new Hive Session ID");
         $hiveFactory = new HiveApiFactory();
         $hive = $hiveFactory->getHiveApi();
 
@@ -53,9 +115,19 @@ class Runner {
         );
 
         if (200 == $response->info->http_code) {
+            $this->_log("Successfully fetched new Hive Session ID");
             $a = json_decode($response->response);
-            echo $a->sessions[0]->id . PHP_EOL;
+            $this->_config->hiveSessionId = $a->sessions[0]->id;
+        } else {
+            $this->_log("Failed to fetch new Hive Session ID");
+            $this->_log(print_r($response->response,true));
         }
+    }
+
+    private function _log($message)
+    {
+        $date = date("r");
+        file_put_contents("php://stderr", "[$date] $message". PHP_EOL);
     }
 }
 
@@ -64,16 +136,57 @@ class HiveApiFactory
 {
     public function __constructor() {}
 
-    public function getHiveApi()
+    public function getHiveApi($sessionId="")
     {
+        $headers = [
+            "Content-Type" => "application/vnd.alertme.zoo-6.1+json",
+            "Accept" => "application/vnd.alertme.zoo-6.1+json",
+            "X-Omnia-Client" => "Hive Web Dashboard"
+        ];
+
+        if (!empty($sessionId)) {
+            $headers["X-Omnia-Access-Token"] = $sessionId;
+        }
+
         return new RestClient([
             "base_url" => "https://api-prod.bgchprod.info:443/omnia",        
+            "headers" => $headers
+        ]);
+    }
+}
+
+class DatadogHelper
+{
+    private $_apiKey;
+    private $_client;
+
+    public function __construct($apiKey)
+    {
+        $this->_apiKey = $apiKey;
+        $this->_client = new RestClient([
+            "base_url" => "https://api.datadoghq.com/api/v1",
             "headers" => [
-                "Content-Type" => "application/vnd.alertme.zoo-6.1+json",
-                "Accept" => "application/vnd.alertme.zoo-6.1+json",
-                "X-Omnia-Client" => "Hive Web Dashboard"
+                "Content-type" => "application/json"
             ]
         ]);
+    }
+
+    // k:v array of metrics
+    public function sendMetrics($metrics) {
+
+        $series = array();
+        foreach ($metrics as $k => $v) {
+            $series[] = [
+                "metric" => $k,
+                "points" => [[time(), $v]],
+                "host" => "home"
+            ];
+        }
+        $response = $this->_client->post("series?api_key=". $this->_apiKey, json_encode(["series" => $series]));
+
+        if (202 != $response->info->http_code) {
+            print_r($response->response);
+        }
     }
 }
 
